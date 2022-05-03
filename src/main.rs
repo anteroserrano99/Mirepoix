4ffea99c65e7fa31141572fa5bdc2f9a82562988
anteroserrano99 <serranoantero13@gmail.com>
2022-05-03 12:49:32 +0200

    Added a method to create a directory depending on the OS you are using

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::fs;
+use std::io::Write;
+use std::env;
 use serde::Deserialize;
 use reqwest::{Error};
 use reqwest::header::USER_AGENT;
@@ -7,6 +10,7 @@ struct Repository {
     name: String,
     html_url: String,
 }
+const directory_name:&str = "anteroserrano99";
 
 #[tokio::main]
 async fn get_repositories() -> Result<Vec<Repository>, Error> {
@@ -32,10 +36,30 @@ fn main(){
 
     let repositories = get_repositories().unwrap();
 
+
+    let os = env::consts::OS;
+    println!("{}", env::consts::OS);
+
+
+    create_directory(os);
+
     for repository in repositories.iter() {
         println!("The name of the repository is: {}",repository.name);
         println!("The repository url is: {}",repository.html_url);
     }
 
 
+}
+
+
+
+
+fn create_directory(os:&str) -> std::io::Result<()> {
+
+    match os {
+        "windows" => fs::create_dir_all(format!("{}{}","C:\\",directory_name))?,
+        _ => fs::create_dir_all(format!("{}{}","/",directory_name))?
+    }
+
+    Ok(())
 }
